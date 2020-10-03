d7acbb9648b195460c6ca55e994ee746c31b4ac3
ttudury <tomastudury@gmail.com>
2020-10-03 00:22:09 -0300

    reduzco rango perimetral

--- src/AppBundle/Repository/EstacionamientoRepository.php
+++ src/AppBundle/Repository/EstacionamientoRepository.php
@@ -10,8 +10,8 @@ class EstacionamientoRepository extends EntityRepository
     {
         $minlat= $latitud-0.01;
         $maxlat= $latitud+0.01;
-        $minlong= $longitud-0.01;
-        $maxlong= $longitud+0.01;
+        $minlong= $longitud-0.005;
+        $maxlong= $longitud+0.005;
         return $this->getEntityManager()
             ->createQuery(
                 "SELECT p FROM AppBundle:estacionamiento p JOIN p.tipoEstacionamiento te where te.codigo = '" . $cod . "' and p.latitud between " . $minlat . " and " . $maxlat . " and p.longitud between " . $minlong . " and " . $maxlong . " "
